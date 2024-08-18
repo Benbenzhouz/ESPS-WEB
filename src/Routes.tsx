74990897274b8d45214085922d9ec57a03d3059f
Benbenzhouz <zhouben168@gmail.com>
2024-08-18 15:42:07 +1000

    web
    web

---
+++ src/Routes.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import QRScannerPage from "./pages/QR/QRScannerPage";
+import Menu from "./pages/Menu/Menu";
+import { DafaultMenuRoutesValues } from "ezpzos.core";
+import Profile from "./pages/Profile/Profile";
+import Kitchen from "./pages/Kitchen/Kitchen";
+import LoginPage from "./pages/Login/LoginPage";
+import SignUpPage from "./pages/SignUp/SignupPage";
+
+const AppRoutes: React.FC = () => (
+	<Routes>
+		{/* Client Routes */}
+		<Route path="/" element={<Home />} />
+		<Route path="signup" element={<SignUpPage />} />
+		<Route path="scan" element={<QRScannerPage />} />
+		<Route path={DafaultMenuRoutesValues.DineInRouteDefaultValue} element={<Menu />} />
+		<Route path={DafaultMenuRoutesValues.TakeAwayRouteDefaultValue} element={<Menu />} />
+		<Route path="profile" element={<Profile />} />
+
+		{/* Business Routes */}
+		<Route path="kitchen" element={<Kitchen />} />
+	</Routes>
+);
+
+export default AppRoutes;
